f28596333a49d398fffdd10abb618834211d0e19
SiddhantJaiswal-0125 <siddhantjaiswal363@gmail.com>
2022-03-09 21:32:18 +0530

    Js

---
+++ Module2/arrays.js
@@ -0,0 +1,7 @@
+//empty array
+let ar = [] ;
+console.log(ar);
+
+//arrray with element
+let ar1 = [1,2,3,"Hello, I am a String", false, 'c', 4.6]; //0 - Based  Indexing
+console.log(ar1);
--- Module2/functions.js
+++ Module2/functions.js
@@ -1,5 +1,31 @@
+//Creating Function in JavaScript
 function sum( a, b)
 {
 return a+b;
 }
 console.log(sum(10, 15));
+
+
+//CREATING inLine Function
+//Functions are called First Class Citizen in JS as We can store them in Inline Variable
+let a = function sub(n1, n2){
+    return n1 - n2;
+}
+console.log(a(10,3));
+
+
+//IIFE : Immediately-Invoked Function Expression
+
+//WITH NO PARAMETER
+(
+    function(){
+         console.log("INSIDE IIFE");
+    }
+) ();
+
+//IIFI with Parameter
+(
+    function(n1, n2){
+    console.log("IIFI WITH PARAMETER "+n1/n2);
+}
+)(10,2); //This line is for calling the same function;
