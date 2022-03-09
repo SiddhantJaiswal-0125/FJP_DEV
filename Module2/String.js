3203ae1853a19a5f96717c35836d5563d8adf022
SiddhantJaiswal-0125 <siddhantjaiswal363@gmail.com>
2022-03-09 21:53:05 +0530

    Js

---
+++ Module2/String.js
@@ -0,0 +1,14 @@
+let str = "My name is Siddhant Siddhant";
+
+//length
+let len = str.length;
+console.log(len);
+ 
+//Sliced Method : KIND OF Substring
+
+let slicedArr = str.slice(0, 18);
+console.log(slicedArr);
+
+//replace method -:> Remove the First Occurence of the Search with the New Value
+let replacedStr = str.replace("Rahul", "Tannu");
+console.log(replacedStr);
