fb4cd33f72063e93d7b5160089ec3f0ca1844d96
SiddhantJaiswal-0125 <siddhantjaiswal363@gmail.com>
2022-03-28 22:15:50 +0530

    update

---
+++ Module2/Interview/executionContext.js
@@ -0,0 +1,11 @@
+var a = 10;
+
+function fun() {
+    var b = 20;
+    console.log(20);
+}
+var c = 20;
+
+console.log(c);
+fun();
+
---
+++ Module2/Interview/var_let_const.js
@@ -0,0 +1,8 @@
+// var a = 20;
+// a = 30;
+// var a = 40;
+// console.log(a);
+
+console.log(a);
+const a = 10;
+// a = 29;
