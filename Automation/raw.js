ffa200e5a378ce63dd51f8e74039d3c36884546d
SiddhantJaiswal-0125 <siddhantjaiswal363@gmail.com>
2022-04-21 21:18:00 +0530

    update

--- Automation/raw.js
+++ Automation/raw.js
@@ -2,7 +2,12 @@ const puppeteer = require('puppeteer');
 
 let browserPromise = puppeteer.launch({headless: false } );
 browserPromise.then(function(browserInstance){
-    let pagePromise  = browser.newPage();
+    let pagePromise  = browserInstance.newPage();
     return pagePromise;
-
-})
+}).then(function(page){
+    console.log("page is opened");
+    let urlPromise = page.goto("https://www.google.co.in/");
+return urlPromise;
+}).then(function(){
+    console.log("Page is Opened");
+});
