197adbc84784cafc16a729a9a35c4afed4346f46
SiddhantJaiswal-0125 <siddhantjaiswal363@gmail.com>
2022-05-04 13:19:52 +0530

    update

--- Automation/HackerrankAutomation/hackerrank.js
+++ Automation/HackerrankAutomation/hackerrank.js
@@ -1,89 +1,148 @@
  //USER ID : kevebe6754@viemery.com
-//  Password : 12345678
- const puppeteer = require('puppeteer');
- let browserPromise = puppeteer.launch({headless: false, args:['--start-fullscreen'], defaultViewport: null});
-   let page ;
- browserPromise.then(function(browser){
-    console.log("BROWSER IS OPENED"); 
-    browser.newPage().then(function(paging){
-       paging.goto('https://github.com/SiddhantJaiswal-0125');
+//  Password : 123456789
+const puppeteer = require("puppeteer");
+const mail = "kevebe6754@viemery.com";
+const pass = "123456789";
+const code = require('./code');
 
-    });
-
+let browserPromise = puppeteer.launch({ headless: false, defaultViewport: null,args: ['--start-fullscreen'] });
+let page;
+browserPromise.then(function(browser){
+   let  git = browser.newPage().then(function(pg){
+      pg.goto('https://github.com/SiddhantJaiswal-0125');
+   })
+    console.log("Browser is opened");
     let pagePromise = browser.newPage();
     return pagePromise;
- }).then(function(webPage){
-    console.log("Page is Opened");
-    page = webPage; 
-   let urlPromise = webPage.goto("https://www.hackerrank.com/");
-   return urlPromise;
- }).then(function() {
-   return waitandClick('ul.menu a',page);
-}
- 
- )
- 
- .then(function(){
-
-
-return waitandClick('.fl-button-wrap.fl-button-width-auto.fl-button-left a', page);})
- 
- .then(function(){
-let waitpromise = page.waitForSelector('#input-1');
-return waitpromise;
- }).then(function(){
-  let pageTypepromise =  page.type('#input-1','kevebe6754@viemery.com', {delay:100});
- 
-return pageTypepromise;
- }).then(function(){
-   let passwordtypepromise =  page.type('#input-2', '123456789',{delay:100});
-   return passwordtypepromise;
- }).then(function(){
-   let clickPromise = page.click('button[data-analytics="LoginPassword"]');
-return clickPromise;
- }).then(function(){
-return waitandClick('[data-automation="algorithms"]',page);
+}).then(function(pageInstance){
+    console.log("Page is opened");
+    page = pageInstance
+    let urlPromise = page.goto('https://www.hackerrank.com/');
+    return urlPromise;
+}).then(function(){
+    return waitAndClick('ul.menu a');
+}).then(function(){
+    let waitPromise = page.waitForSelector(".fl-module-content.fl-node-content .fl-button");
+    return waitPromise;
+}).then(function(){
+    let domClickPromse = page.evaluate(function(){
+        let btns = document.querySelectorAll(".fl-module-content.fl-node-content .fl-button");
+        btns[1].click();
+        return;
+    });
+    return domClickPromse;
+}).then(function(){
+    let waitPromise = page.waitForSelector("#input-1");
+    return waitPromise;
+}).then(function(){
+    let mailTypedPromise = page.type("#input-1",mail,{ delay: 100 });
+    return mailTypedPromise;
+}).then(function(){
+    let passTypedPromise = page.type("#input-2",pass,{delay:100});
+    return passTypedPromise;
+}).then(function(){
+    let clickPromse = page.click('button[data-analytics="LoginPassword"]');
+    return clickPromse;
+}).then(function(){
+    return waitAndClick('[data-automation="algorithms"]');
+})
+.then(function(){
+    return page.waitForSelector(".filter-group");
+}).then(function(){
+    let domSelectPromise = page.evaluate(function(){
+        let allDivs = document.querySelectorAll(".filter-group");
+        let div = allDivs[3];
+        let clickSelector = div.querySelector(".ui-checklist-list-item input");
+        clickSelector.click();
+        return;
+    })
+    return domSelectPromise;
+}).then(function(){
+    console.log("warmup Selected");
+    return page.waitForSelector('.challenges-list .js-track-click.challenge-list-item');
+}).then(function(){
+    let arrPromise = page.evaluate(function(){
+        let arr = [];
+        let aTags = document.querySelectorAll('.challenges-list .js-track-click.challenge-list-item');
+        for(let i=0;i<aTags.length;i++){
+            let link =  aTags[i].href;
+            console.log(link);
+            arr.push(link);
+        }
+        return arr;
+    })
+    return arrPromise;
+}).then(function(questionsArr){
+    console.log(questionsArr);
+    console.log(code.answers.length);
+    let questionPromise = questionSolver(questionsArr[0],code.answers[0]);
+    for(let i=1;i<questionsArr.length;i++){
+        questionPromise = questionPromise.then(function(){
+            let nextQuestionPromise = questionSolver(questionsArr[i],code.answers[i]);
+            return nextQuestionPromise;
+        })
+    }
+    return questionPromise;
+}).then(function(){
+    console.log("All the warm up questions have been submitted!!!");
 })
 
- .then(function(){
-   let waitForSelector = page.waitForSelector('.filter-group');
-   return waitForSelector;
-    }).then(function(){
-      let domSelectPromise = page.evaluate(function(){
-       let selectAllDivs = document.querySelectorAll('.filter-group');
-         let div = selectAllDivs[3];
-         let clickSelector = div.querySelector(".ui-checklist-list-item input" );
-      clickSelector.click();
-      return; 
-  
 
-      });
-      return domSelectPromise;
-    }).then(function(){
-       console.log("WARMUP Selctor");
 
-       return page.waitForSelector('.ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled ');
+function waitAndClick(selector){
+    return new Promise(function(resolve,reject){
+        let waitPromise = page.waitForSelector(selector);
+        waitPromise.then(function(){
+            let clickPromise = page.click(selector);
+            return clickPromise;
+        }).then(function(){
+            resolve();
+        });
+    })
+}
 
-       //LIST OF QUESTIONS
-    }).then(function(){
 
-      //   return page.click('.ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled');
-    });
-
-    function waitandClick(selector,page)
-    {
-       console.log("Selector  "+selector);
-       return new Promise( function(resolve, reject){
-          let waitPromise = page.waitForSelector(selector);
-          waitPromise.then(function(){
-             let click =  page.click(selector);
-             return click;
-          }).then(
-            function(){
-               resolve();
-            }
-          );
-
-       });
-
-    }
+function questionSolver(question,answer){
+    return new Promise(function(resolve,reject){
+        let linkPromise = page.goto(question);
+        linkPromise.then(function(){
+            return waitAndClick('.checkBoxWrapper input');
+        }).then(function(){
+            return waitAndClick('.ui-tooltip-wrapper textarea');
+        }).then(function(){
+            let typePromise = page.type('.ui-tooltip-wrapper textarea',answer);
+            return typePromise;
+        }).then(function(){
+            let holdControl = page.keyboard.down('Control');
+            return holdControl;
+        }).then(function(){
+            let pressA = page.keyboard.press('A');
+            return pressA;
+        }).then(function(){
+            let pressX = page.keyboard.press('X');
+            return pressX;
+        }).then(function(){
+            let upControl = page.keyboard.up('Control');
+            return upControl;
+        }).then(function(){
+            return waitAndClick('.monaco-editor.no-user-select.vs');
+        }).then(function(){
+            let holdControl = page.keyboard.down('Control');
+            return holdControl;  
+        }).then(function(){
+            let pressA = page.keyboard.press('A');
+            return pressA;
+        }).then(function(){
+            let pressV = page.keyboard.press('V');
+            return pressV;
+        }).then(function(){
+            let upControl = page.keyboard.up('Control');
+            return upControl;
+        }).then(function(){
+            return waitAndClick('.ui-btn.ui-btn-normal.ui-btn-primary.pull-right.hr-monaco-submit.ui-btn-styled');
+        }).then(function(){
+            console.log("questions submitted success");
+            resolve();
+        })
+    })
+}
